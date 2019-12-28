455ae81b52508d6e7c422116dcb9a9e95749c8d3
Goldenstriker <ndparab240@gmail.com>
2019-12-28 19:02:25 +0530

    1 file modified

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-//import { AuthenticationService, UserService } from "./_services";
 import { AuthenticationService, UserService } from "../_services";
 import { User } from "../_models";
 
