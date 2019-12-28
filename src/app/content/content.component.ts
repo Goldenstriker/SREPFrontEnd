72b6e19f7af5213af5d1e692996a33d4bcf85fab
Goldenstriker <ndparab240@gmail.com>
2019-12-28 18:46:00 +0530

    2 files modified

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthenticationService, UserService } from "./_services";
+import { User } from "./_models";
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  selector: "app-content",
+  templateUrl: "./content.component.html",
+  styleUrls: ["./content.component.css"]
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  currentUser: User;
+  currentLoggedInUser: string;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => {
+      if (x) {
+        this.currentUser = x;
+        this.userService.getCurrentLoggedIn().subscribe(y => {
+          this.currentLoggedInUser = y.user;
+          console.log(y.user);
+        });
+      }
+    });
   }
-
+  ngOnInit() {}
 }
