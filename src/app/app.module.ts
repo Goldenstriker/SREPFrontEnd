d23daa012f5461d59883188dd871473f75a0ebb0
Goldenstriker <ndparab240@gmail.com>
2019-12-28 12:32:08 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar';
-import { FooterComponent } from './footer';
-import { ContentComponent } from './content';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar";
+import { FooterComponent } from "./footer";
+import { ContentComponent } from "./content";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, NavbarComponent, FooterComponent, ContentComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContentComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
