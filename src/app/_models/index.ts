05b82deb82950e4654fa23d2131f31c15171278c
Goldenstriker <ndparab240@gmail.com>
2019-12-30 21:58:36 +0530

    2 files modified

--- src/app/_models/index.ts
+++ src/app/_models/index.ts
@@ -1,1 +1,7 @@
 export * from './user';
+export * from './country';
+export * from './state';
+export * from './city';
+export * from './property';
+export * from './propertytype';
+export * from './propertystatus';
--- src/app/_services/master.service.ts
+++ src/app/_services/master.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Constant } from "../constants";
+import {Country, State, City} from "../models";
 @Injectable({ providedIn: "root" })
 export class MasterService {
   constructor(private http: HttpClient) {}
   public baseURL : string = Constant.SiteURL;
   getAllCountry(){
-    return this.http.get(this.baseURL)
+    return this.http.get<Country[]>(this.baseURL)
   }
 }
