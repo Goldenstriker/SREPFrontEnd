32544b8f4298b89b0b05a76bfd8352803d90510f
Goldenstriker <ndparab240@gmail.com>
2019-12-30 16:20:44 +0530

    1 file modified

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export class Constant{
 
-public static SiteURL :string = "https://srepfrontend111.stackblitz.io";
-public static AuthenticationURL :string = "https://srepfrontend111.stackblitz.io/api-token-auth/";
+public static SiteURL :string = "https://srepbackend-1.goldenstriker.repl.co";
+public static AuthenticationURL :string = "https://srepbackend-1.goldenstriker.repl.co/api-token-auth/";
 }
