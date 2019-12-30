729fcfff02f2c876137c91e4c53dc95fa57affe3
Goldenstriker <ndparab240@gmail.com>
2019-12-30 15:08:20 +0530

    1 file modified

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,28 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { User } from '../_models';
-import {SiteURL} from '../constants';
+import { User } from "../_models";
+import { SiteURL } from "../constants";
 
-
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class UserService {
-    constructor(private http: HttpClient) { }
-    public baseURL: string = SiteURL;
-    getAll() {
-        console.log(SiteURL);
-        return this.http.get<User[]>(this.baseURL+`/users/`);
-    }
+  constructor(private http: HttpClient) {}
+  public baseURL: string = SiteURL;
+  getAll() {
+    console.log(SiteURL);
+    return this.http.get<User[]>(this.baseURL + `/users/`);
+  }
 
-    register(user: User) {
-        return this.http.post(this.baseURL+`/register/`, user);
-    }
+  register(user: User) {
+    console.log(SiteURL);
+    return this.http.post(this.baseURL + `/register/`, user);
+  }
 
-    delete(id: number) {
-        return this.http.delete(this.baseURL+`/users/${id}`);
-    }
+  delete(id: number) {
+    return this.http.delete(this.baseURL + `/users/${id}`);
+  }
 
-    getCurrentLoggedIn(){
-        return this.http.get<User>(this.baseURL+`/current_user/`);
-    }
+  getCurrentLoggedIn() {
+    return this.http.get<User>(this.baseURL + `/current_user/`);
+  }
 }
