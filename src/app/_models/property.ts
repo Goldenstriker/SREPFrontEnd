d33a1a59a1cf0f665712504474c2f006542d4d2d
Goldenstriker <ndparab240@gmail.com>
2019-12-30 17:20:14 +0530

    1 file modified

--- src/app/_models/property.ts
+++ src/app/_models/property.ts
@@ -1,16 +1,18 @@
-import {Country} from './country';
-import {State} from './state';
-import {City} from './city';
-export class Property{
-        ID: number;
-        Name: string;
-        Description: string;
-        No_Of_BedRooms: number;
-        No_Of_BathRooms:number;
-        No_Of_Floors : number;
-        Country: Country;
-        State: State;
-        City: City;
-        "Property_Status": 2,
-        "Property_Type": 1
+import { Country } from "./country";
+import { State } from "./state";
+import { City } from "./city";
+import { PropertyStatus } from "./propertystatus";
+import { PropertyType } from "./propertytype";
+export class Property {
+  ID: number;
+  Name: string;
+  Description: string;
+  No_Of_BedRooms: number;
+  No_Of_BathRooms: number;
+  No_Of_Floors: number;
+  Country: Country;
+  State: State;
+  City: City;
+  Property_Status: PropertyStatus;
+  Property_Type: PropertyType;
 }
