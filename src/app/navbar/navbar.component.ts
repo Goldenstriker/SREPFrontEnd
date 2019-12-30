d2afd61788d401bba90c370b4c3d184c64d91ab4
Goldenstriker <ndparab240@gmail.com>
2019-12-30 14:34:09 +0530

    2 files modified

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from "@angular/router";
+import { AuthenticationService, UserService } from "../_services";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService) { }
 
   ngOnInit() {
   }
-
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(["/login"]);
+  }
 }
