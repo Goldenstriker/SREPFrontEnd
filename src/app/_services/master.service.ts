8ded216a86ec1cdd16ed31e7784e34e01d250077
Goldenstriker <ndparab240@gmail.com>
2019-12-30 22:01:11 +0530

    1 file modified

--- src/app/_services/master.service.ts
+++ src/app/_services/master.service.ts
@@ -7,6 +7,12 @@ export class MasterService {
   constructor(private http: HttpClient) {}
   public baseURL : string = Constant.SiteURL;
   getAllCountry(){
-    return this.http.get<Country[]>(this.baseURL)
+    return this.http.get<Country[]>(this.baseURL+'/country/');
+  }
+  getAllStateByCountry(){
+    return this.http.get<State[]>(this.baseURL+'/state/');
+  }
+  getAllCityByState(){
+    return this.http.get<City[]>(this.baseURL+'/city/');
   }
 }
