ab38715c72d784e58607ab76baf805806892846b
Goldenstriker <ndparab240@gmail.com>
2020-01-02 18:46:38 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar";
 import { FooterComponent } from "./footer";
 import { appRoutingModule } from "./app.routing";
-import { JwtInterceptor, ErrorInterceptor } from "./_helpers";
+import { JwtInterceptor, ErrorInterceptor, LoaderInterceptor } from "./_helpers";
 import { ContentComponent } from "./content";
 import { RegisterComponent } from "./register";
 import { LoginComponent } from "./login";
 import { AlertComponent } from "./_components";
 import { HomeComponent } from "./home";
 import { RegisterpropertyComponent } from './registerproperty';
-import { PropertyService } from './_services';
+import { LoaderService } from './_services';
 import { PropertyComponent } from './property';
 @NgModule({
   imports: [
@@ -37,7 +37,8 @@ import { PropertyComponent } from './property';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true, providers: [PropertyService] }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true, providers: [PropertyService] },
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true, providers:[LoaderService] }
   ],
   bootstrap: [AppComponent]
 })
