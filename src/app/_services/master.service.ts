3ee4ef6bf431bf59c460017ed5fa08d279ad64c7
Goldenstriker <ndparab240@gmail.com>
2020-01-02 14:29:19 +0530

    1 file modified

--- src/app/_services/master.service.ts
+++ src/app/_services/master.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Constant } from "../constants";
-import {Country, State, City} from "../models";
+import { Country, State, City, PropertyType, PropertyStatus } from "../models";
 @Injectable({ providedIn: "root" })
 export class MasterService {
   constructor(private http: HttpClient) {}
-  public baseURL : string = Constant.SiteURL;
-  getAllCountry(){
-    return this.http.get<Country[]>(this.baseURL+'/country/');
+  public baseURL: string = Constant.SiteURL;
+  getAllCountry() {
+    return this.http.get<Country[]>(this.baseURL + "/country/");
   }
-  getAllStateByCountry(){
-    return this.http.get<State[]>(this.baseURL+'/state/');
+  getAllStateByCountry() {
+    return this.http.get<State[]>(this.baseURL + "/state/");
   }
-  getAllCityByState(){
-    return this.http.get<City[]>(this.baseURL+'/city/');
+  getAllCityByState() {
+    return this.http.get<City[]>(this.baseURL + "/city/");
+  }
+  getPropertyType() {
+    return this.http.get<PropertyType[]>(this.baseURL + "/city/");
+  }
+  getPropertyStatus() {
+    return this.http.get<PropertyStatus[]>(this.baseURL + "/city/");
   }
 }
