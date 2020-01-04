46397a0f93d055cf0e64d21a80b0a571ba719b78
Goldenstriker <ndparab240@gmail.com>
2020-01-04 13:30:21 +0530

    2 files modified

--- src/app/_helpers/filter.pipe.ts
+++ src/app/_helpers/filter.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class FilterPipe implements PipeTransform {
   transform(items: any, searchText: any): any {
     if (!items) return [];
+    searchText = JSON.parse(searchText);
     if (searchText.Country.length==0 || searchText.State.length==0 || searchText.City.length==0) return items;
     //if (searchText.length==0) return items;
     //searchText = searchText.toLowerCase();
