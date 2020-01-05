53047d03f91a3e0eb7d0ae504e45b233bb619726
Goldenstriker <ndparab240@gmail.com>
2020-01-05 12:46:47 +0530

    4 files modified

--- src/app/_services/property.service.ts
+++ src/app/_services/property.service.ts
@@ -7,22 +7,18 @@ export class PropertyService {
   constructor(private http: HttpClient) {}
   public baseURL: string = Constant.SiteURL;
   register(property: Property) {
-    
     return this.http.post(this.baseURL + `/properties/`, property);
   }
 
   getAll() {
-    
     return this.http.get<Property[]>(this.baseURL + `/properties/`);
   }
 
   get(id: number) {
-    
     return this.http.get<Property>(this.baseURL + `/properties/` + id);
   }
 
-  update(property: Property,id:string) {
-    
-    return this.http.put(this.baseURL + `/properties/`+ id+'/', property);
+  update(property: Property, id: string) {
+    return this.http.put(this.baseURL + `/properties/` + id + "/", property);
   }
 }
