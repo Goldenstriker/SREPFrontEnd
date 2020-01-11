d5b6b359376606bc259ae265e4e149e11e9951ac
Goldenstriker <ndparab240@gmail.com>
2020-01-11 23:28:28 +0530

    1 file modified

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export class Constant{
 
-public static SiteURL :string = "https://srepbackend-1.goldenstriker.repl.co";
-public static AuthenticationURL :string = "https://srepbackend-1.goldenstriker.repl.co/api-token-auth/";
+public static SiteURL :string = "https://srepbackend.goldenstriker.repl.co";
+public static AuthenticationURL :string = "https://srepbackend.goldenstriker.repl.co//api-token-auth/";
 }
