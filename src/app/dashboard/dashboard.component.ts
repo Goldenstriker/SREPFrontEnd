24bea0b4e1f906fc2d511d0f4b3c99c5f75284af
Goldenstriker <ndparab240@gmail.com>
2020-01-14 00:12:08 +0530

    1 file modified

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 
+import {
+  UserService,
+  AuthenticationService,
+  MasterService,
+  PropertyService
+} from "../_services";
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  currentUser: User;
+  currentUserSubscription: Subscription;
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private masterService: MasterService,
+    private propertyService: PropertyService
+  ) {
+    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(
+      user => {
+        this.currentUser = user;
+      }
+    );
+    this.userService.getCurrentLoggedIn().subscribe((y: any) => {
+      this.currentLoggedInUser = y.user_id;
+    });
   }
 
+  ngOnInit() {}
 }
