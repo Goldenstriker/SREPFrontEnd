c13df8da95eec355784264d4a54e95335c04a98d
Goldenstriker <ndparab240@gmail.com>
2020-01-15 18:39:24 +0530

    5 files modified

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -16,26 +16,71 @@ export class ManagementComponent implements OnInit {
   ngOnInit() {
     this.loadAllProperty();
     let chart = new CanvasJS.Chart("chartContainer1", {
-		theme: "light2",
-		animationEnabled: true,
-		exportEnabled: true,
-		title:{
-			text: "Monthly Property Data"
-		},
-		data: [{
-			type: "pie",
-			showInLegend: true,
-			toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
-			indexLabel: "{name} - #percent%",
-			dataPoints: [
-				{ y: 450, name: "Sold" },
-				{ y: 500, name: "Available" },
-				{ y: 300, name: "On Hold" }
-			]
-		}]
-	});
-		
-	chart.render();
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: false,
+      title: {
+        text: ""
+      },
+      data: [
+        {
+          type: "pie",
+          showInLegend: true,
+          toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+          indexLabel: "{name} - #percent%",
+          dataPoints: [
+            { y: 450, name: "Sold" },
+            { y: 500, name: "Available" },
+            { y: 300, name: "On Hold" }
+          ]
+        }
+      ]
+    });
+    let chart1 = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: false,
+      title: {
+        text: ""
+      },
+      data: [
+        {
+          type: "pie",
+          showInLegend: true,
+          toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+          indexLabel: "{name} - #percent%",
+          dataPoints: [{ y: 450, name: "Sell" }, { y: 500, name: "Rent" }]
+        }
+      ]
+    });
+    let chart2 = new CanvasJS.Chart("chartContainer2", {
+      animationEnabled: true,
+      title: {
+        text: "Email Categories",
+        horizontalAlign: "left"
+      },
+      data: [
+        {
+          type: "doughnut",
+          startAngle: 60,
+          //innerRadius: 60,
+          indexLabelFontSize: 17,
+          indexLabel: "{label} - #percent%",
+          toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+          dataPoints: [
+            { y: 67, label: "Inbox" },
+            { y: 28, label: "Archives" },
+            { y: 10, label: "Labels" },
+            { y: 7, label: "Drafts" },
+            { y: 15, label: "Trash" },
+            { y: 6, label: "Spam" }
+          ]
+        }
+      ]
+    });
+    chart2.render();
+    chart1.render();
+    chart.render();
   }
   private loadAllProperty() {
     this.propertyService.getAll().subscribe(properties => {
@@ -43,7 +88,7 @@ export class ManagementComponent implements OnInit {
       let data = properties.map(data => {
         return { x: data.AreaSqFt, y: data.Price };
       });
-      let chart = new CanvasJS.Chart("chartContainer", {
+      /*let chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         exportEnabled: false,
         zoomEnabled: true,
@@ -67,7 +112,7 @@ export class ManagementComponent implements OnInit {
           }
         ]
       });
-      chart.render();
+      chart.render();*/
     });
   }
 }
