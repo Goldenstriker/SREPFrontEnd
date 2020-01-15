4bc86c62f61355b870e24fac255c742536c18344
Goldenstriker <ndparab240@gmail.com>
2020-01-15 12:38:35 +0530

    2 files modified

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -1,39 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import * as CanvasJS from 'canvasjs';
-
+import { Component, OnInit } from "@angular/core";
+import * as CanvasJS from "canvasjs";
+import { Property } from "../_models";
+import { PropertyService } from "../_services";
+import { map } from "rxjs/operators";
 @Component({
-  selector: 'app-management',
-  templateUrl: './management.component.html',
-  styleUrls: ['./management.component.css']
+  selector: "app-management",
+  templateUrl: "./management.component.html",
+  styleUrls: ["./management.component.css"]
 })
 export class ManagementComponent implements OnInit {
+  properties: Property[] = [];
+  constructor(private propertyService: PropertyService) {}
 
-  constructor() { }
+  ngOnInit() {}
+  private loadAllProperty() {
+    this.propertyService.getAll().subscribe(properties => {
+      // properties;
 
-  ngOnInit() {
-    let chart = new CanvasJS.Chart("chartContainer", {
-		animationEnabled: true,
-		exportEnabled: true,
-		title: {
-			text: "Basic Column Chart in Angular"
-		},
-		data: [{
-			type: "column",
-			dataPoints: [
-				{ y: 71, label: "Apple" },
-				{ y: 55, label: "Mango" },
-				{ y: 50, label: "Orange" },
-				{ y: 65, label: "Banana" },
-				{ y: 95, label: "Pineapple" },
-				{ y: 68, label: "Pears" },
-				{ y: 28, label: "Grapes" },
-				{ y: 34, label: "Lychee" },
-				{ y: 14, label: "Jackfruit" }
-			]
-		}]
-	});
-		
-	chart.render();
+      let chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        exportEnabled: true,
+        zoomEnabled: true,
+        title: {
+          text: "Real Estate Rates"
+        },
+        axisX: {
+          title: "Area (in sq. ft)",
+          minimum: 790,
+          maximum: 2260
+        },
+        axisY: {
+          title: "Price (in USD)",
+          valueFormatString: "$#,##0k"
+        },
+        data: [
+          {
+            type: "scatter",
+            toolTipContent: "<b>Area: </b>{x} sq.ft<br/><b>Price: </b>${y}k",
+            dataPoints: properties.map(data => {
+              return { x: data.AreaSqFt, y: data.Price };
+            })
+          }
+        ]
+      });
+      chart.render();
+    });
   }
-
 }
