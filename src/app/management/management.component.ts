d393090aee6e8aa72ae94eac3e5e0486f87d878a
Goldenstriker <ndparab240@gmail.com>
2020-01-15 18:45:55 +0530

    1 file modified

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -22,7 +22,12 @@ export class ManagementComponent implements OnInit {
       title: {
         text: ""
       },
-      
+      legend: {
+        fontFamily: "calibri",
+        fontSize: 14,
+        horizontalAlign: "left",
+        verticalAlign: "center"
+      },
       data: [
         {
           type: "pie",
@@ -61,38 +66,6 @@ export class ManagementComponent implements OnInit {
         }
       ]
     });
-    let chart2 = new CanvasJS.Chart("chartContainer2", {
-      animationEnabled: true,
-      title: {
-        text: "Email Categories",
-        horizontalAlign: "left"
-      },
-      legend: {
-        fontFamily: "calibri",
-        fontSize: 14,
-        horizontalAlign: "left",
-        verticalAlign: "center"
-      },
-      data: [
-        {
-          type: "doughnut",
-          startAngle: 60,
-          //innerRadius: 60,
-          indexLabelFontSize: 17,
-          indexLabel: "{label} - #percent%",
-          toolTipContent: "<b>{label}:</b> {y} (#percent%)",
-          dataPoints: [
-            { y: 67, label: "Inbox" },
-            { y: 28, label: "Archives" },
-            { y: 10, label: "Labels" },
-            { y: 7, label: "Drafts" },
-            { y: 15, label: "Trash" },
-            { y: 6, label: "Spam" }
-          ]
-        }
-      ]
-    });
-    chart2.render();
     chart1.render();
     chart.render();
   }
