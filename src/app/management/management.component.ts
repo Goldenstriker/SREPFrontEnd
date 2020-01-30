bb224920f5c63c5ce55d2354459a5dfe1c07d459
Goldenstriker <ndparab240@gmail.com>
2020-01-30 10:35:03 +0530

    1 file modified

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -14,144 +14,144 @@ export class ManagementComponent implements OnInit {
   property_purposeChartData = [];
   property_typeChartData = [];
   property_statusChartData = [];
+  property_city_count = [];
   constructor(private propertyService: PropertyService) {}
 
   ngOnInit() {
     this.loadAllProperty();
-    
   }
   private loadAllProperty() {
     this.propertyService.getChartData().subscribe(data => {
-        let property_type = JSON.parse(data["property_type"]);
-        let property_purpose = JSON.parse(data["property_purpose"]);
-        let property_status = JSON.parse(data["property_status"]);
-        console.log(property_type);
-        
-        Object.keys(property_type).forEach(x=>{
-         this.property_typeChartData.push({ y: property_type[x], name: x });
-        });
-      
-        Object.keys(property_purpose).forEach(x=>{
-          this.property_purposeChartData.push({ y: property_purpose[x], name: x });
-        });
-        Object.keys(property_status).forEach(x=>{
-          this.property_statusChartData.push({ y: property_status[x], name: x });
-        });
-      let chart = new CanvasJS.Chart("chartContainer1", {
-      theme: "light2",
-      animationEnabled: true,
-      exportEnabled: false,
-      title: {
-        text: ""
-      },
-      legend: {
-        fontFamily: "calibri",
-        fontSize: 14,
-        horizontalAlign: "left",
-        verticalAlign: "center"
-      },
-      data: [
-        {
-          type: "pie",
-          showInLegend: true,
-          toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
-          indexLabel: "{name} - #percent%",
-          dataPoints: this.property_typeChartData
-        }
-      ]
-    });
-    let chart1 = new CanvasJS.Chart("chartContainer", {
-      theme: "light2",
-      animationEnabled: true,
-      exportEnabled: false,
-      title: {
-        text: ""
-      },
-      legend: {
-        fontFamily: "calibri",
-        fontSize: 14,
-        horizontalAlign: "left",
-        verticalAlign: "center"
-      },
-      data: [
-        {
-          legendMarkerType: "square",
-          type: "pie",
-          showInLegend: true,
-          toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
-          indexLabel: "{name} - #percent%",
-          dataPoints: this.property_purposeChartData
-        }
-      ]
-    });
-    let chart3 = new CanvasJS.Chart("chartContainer3", {
-      theme: "light2",
-      animationEnabled: true,
-      exportEnabled: false,
-      title: {
-        text: ""
-      },
-      legend: {
-        fontFamily: "calibri",
-        fontSize: 14,
-        horizontalAlign: "left",
-        verticalAlign: "center"
-      },
-      data: [
-        {
-          legendMarkerType: "square",
-          type: "pie",
-          showInLegend: true,
-          toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
-          indexLabel: "{name} - #percent%",
-          dataPoints: this.property_statusChartData
-        }
-      ]
-    });
-    let usersStateBarChart = new CanvasJS.Chart("users-state-bar-chart", {
-      animationDuration: 800,
-      animationEnabled: true,
-      backgroundColor: "transparent",
-      colorSet: "customColorSet",
-      axisX: {
-        labelFontColor: "#717171",
-        labelFontSize: 18,
-        lineThickness: 0,
-        tickThickness: 0
-      },
-      axisY: {
-        gridThickness: 0,
-        lineThickness: 0,
-        tickThickness: 0,
-        valueFormatString: " "
-      },
-      toolTip: {
-        cornerRadius: 0,
-        fontStyle: "normal"
-      },
-      data: [
-        {
-          indexLabelFontColor: "#717171",
-          indexLabelFontFamily: "calibri",
-          indexLabelFontSize: 18,
-          indexLabelPlacement: "outside",
-          type: "bar",
-          dataPoints: [
-            { y: 16, label: "Others" },
-            { y: 4, label: "Pennsylvania" },
-            { y: 5, label: "Florida" },
-            { y: 7, label: "Texas" },
-            { y: 11, label: "New York" },
-            { y: 12, label: "California" }
-          ]
-        }
-      ]
-    });
+      let property_type = JSON.parse(data["property_type"]);
+      let property_purpose = JSON.parse(data["property_purpose"]);
+      let property_status = JSON.parse(data["property_status"]);
+      let city_count = JSON.parse(data["city_count"]);
+      console.log(property_type);
 
-    usersStateBarChart.render();
-    chart3.render();
-    chart1.render();
-    chart.render();
+      Object.keys(property_type).forEach(x => {
+        this.property_typeChartData.push({ y: property_type[x], name: x });
+      });
+
+      Object.keys(property_purpose).forEach(x => {
+        this.property_purposeChartData.push({
+          y: property_purpose[x],
+          name: x
+        });
+      });
+      Object.keys(property_status).forEach(x => {
+        this.property_statusChartData.push({ y: property_status[x], name: x });
+      });
+      Object.keys(city_count).forEach(x => {
+        this.property_city_count.push({ y: city_count[x], label: x });
+      });
+      let chart = new CanvasJS.Chart("chartContainer1", {
+        theme: "light2",
+        animationEnabled: true,
+        exportEnabled: false,
+        title: {
+          text: ""
+        },
+        legend: {
+          fontFamily: "calibri",
+          fontSize: 14,
+          horizontalAlign: "left",
+          verticalAlign: "center"
+        },
+        data: [
+          {
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+            indexLabel: "{name} - #percent%",
+            dataPoints: this.property_typeChartData
+          }
+        ]
+      });
+      let chart1 = new CanvasJS.Chart("chartContainer", {
+        theme: "light2",
+        animationEnabled: true,
+        exportEnabled: false,
+        title: {
+          text: ""
+        },
+        legend: {
+          fontFamily: "calibri",
+          fontSize: 14,
+          horizontalAlign: "left",
+          verticalAlign: "center"
+        },
+        data: [
+          {
+            legendMarkerType: "square",
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+            indexLabel: "{name} - #percent%",
+            dataPoints: this.property_purposeChartData
+          }
+        ]
+      });
+      let chart3 = new CanvasJS.Chart("chartContainer3", {
+        theme: "light2",
+        animationEnabled: true,
+        exportEnabled: false,
+        title: {
+          text: ""
+        },
+        legend: {
+          fontFamily: "calibri",
+          fontSize: 14,
+          horizontalAlign: "left",
+          verticalAlign: "center"
+        },
+        data: [
+          {
+            legendMarkerType: "square",
+            type: "pie",
+            showInLegend: true,
+            toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+            indexLabel: "{name} - #percent%",
+            dataPoints: this.property_statusChartData
+          }
+        ]
+      });
+      let usersStateBarChart = new CanvasJS.Chart("users-state-bar-chart", {
+        animationDuration: 800,
+        animationEnabled: true,
+        backgroundColor: "transparent",
+        colorSet: "customColorSet",
+        axisX: {
+          labelFontColor: "#717171",
+          labelFontSize: 18,
+          lineThickness: 0,
+          tickThickness: 0
+        },
+        axisY: {
+          gridThickness: 0,
+          lineThickness: 0,
+          tickThickness: 0,
+          valueFormatString: " "
+        },
+        toolTip: {
+          cornerRadius: 0,
+          fontStyle: "normal"
+        },
+        data: [
+          {
+            indexLabelFontColor: "#717171",
+            indexLabelFontFamily: "calibri",
+            indexLabelFontSize: 18,
+            indexLabelPlacement: "outside",
+            type: "bar",
+            dataPoints: this.property_city_count
+          }
+        ]
+      });
+
+      usersStateBarChart.render();
+      chart3.render();
+      chart1.render();
+      chart.render();
     });
   }
 }
