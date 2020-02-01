a281ae39c7433c734e0a168dfc392046768f1a04
Goldenstriker <ndparab240@gmail.com>
2020-02-01 11:38:48 +0530

    1 file modified, 1 added

--- src/app/propertydetails/propertydetails.component.ts
+++ src/app/propertydetails/propertydetails.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { first } from "rxjs/operators";
+import {
+  AlertService,
+  UserService,
+  AuthenticationService,
+  PropertyService
+} from "../_services";
+import { Property } from "../_models";
 @Component({
-  selector: 'app-propertydetails',
-  templateUrl: './propertydetails.component.html',
-  styleUrls: ['./propertydetails.component.css']
+  selector: "app-propertydetails",
+  templateUrl: "./propertydetails.component.html",
+  styleUrls: ["./propertydetails.component.css"]
 })
 export class PropertydetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  recordID: string = "";
+  property: Property;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private alertService: AlertService,
+    private propertyService: PropertyService
+  ) {
+    this.route.params.subscribe(params => {
+      if (params && params.id) {
+        this.recordID = params.id;
+        this.propertyService.get(params.id).subscribe(property => {
+          this.property = property;
+        });
+      }
+    });
   }
 
+  ngOnInit() {}
 }
